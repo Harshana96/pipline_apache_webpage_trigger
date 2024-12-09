34760621f02abd2c93f77a48d961f805dbb34a7b
Your Name <you@example.com>
2024-12-09 15:18:42 +0530

    change groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -20,15 +20,17 @@ pipeline {
         stage('Copy to Remote Server') {
             steps {
                 script {
-                    sshagent(['ssh-connection-apache-server']) {
+                    sshagent(['ssh-apche']) {
                         sh '''
                             echo "+++++++++Copying index.html to remote server+++++++++"
-                            scp -o StrictHostKeyChecking=no -i /var/lib/jenkins/workspace/test_pipline_1@tmp/private_key_1699415439007742700.key index.html hashj@172-31-22-226:/tmp/index.html
+                            scp -o StrictHostKeyChecking=no index.html hashj@172-31-22-226:/tmp/index.html
+
                             echo "+++++++++Taking root access and moving file+++++++++"
-                            ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/workspace/test_pipline_1@tmp/private_key_1699415439007742700.key hashj@172-31-22-226 \
+                            ssh -o StrictHostKeyChecking=no hashj@172-31-22-226 \
                                 'sudo -i bash -c "mv /tmp/index.html /var/www/html/index.html && systemctl reload apache2.service"'
                         '''
                     }
+
                 }
             }
         }
