0b85786abb5761a09b663c1425401e5de66e6649
Your Name <you@example.com>
2024-12-09 14:21:30 +0530

    change groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -18,13 +18,17 @@ pipeline {
         }
     }
 
-    // stage('Copy to Remote Server') {
-    //         steps {
-    //             script {
-    //                 sshagent(['ssh-connection-apache-server']) {
-                       
-    //                 }
-    //             }
-    //         }
-    //     }
+    stage('Copy to Remote Server') {
+            steps {
+                script {
+                    sshagent(credentials: ['ssh-connection-apache-server']) {
+                        sh '''
+                            [ -d ~/.ssh ] || mkdir ~/.ssh && chmod 0700 ~/.ssh
+                            ssh-keyscan -t rsa,dsa ip-172-31-22-226 >> ~/.ssh/known_hosts
+                            ssh harshana-1@ip-172-31-22-226 ...
+                        '''
+                    }
+                }
+            }
+        }
 }
