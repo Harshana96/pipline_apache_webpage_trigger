f33847b9519b19a0a61e6448b1a28292724e1f2b
Your Name <you@example.com>
2024-12-09 13:37:18 +0530

    change groovy file - 1

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,28 +1,18 @@
 pipeline {
     agent any
 
-    environment {
-        REMOTE_SERVER = 'ubuntu@ip-172-31-22-226'
-        PRIVATE_KEY_PATH = '~/.ssh/id_rsa'
-    }
-
     stages {
         stage('Check Local File') {
             steps {
                 script {
-                    sshagent(credentials: ['your-ssh-credentials-id']) {
-                        // Copy the file
-                        sh '''
-                            echo "+++++++++Copying index.html to remote server+++++++++"
-                            scp -o StrictHostKeyChecking=no -i ${PRIVATE_KEY_PATH} index.html ${REMOTE_SERVER}:/tmp/index.html
-                        '''
-                        // SSH and move file
-                        sh '''
-                            echo "+++++++++Taking root access and moving file+++++++++"
-                            ssh -o StrictHostKeyChecking=no -i ${PRIVATE_KEY_PATH} ${REMOTE_SERVER} \
-                                'sudo mv /tmp/index.html /var/www/html/index.html && sudo systemctl reload apache2.service'
-                        '''
-                    }
+                    sh '''
+                        echo "+++++++++Current directory:+++++++++"
+                        pwd
+                        echo "+++++++++Listing files:+++++++++"
+                        ls -l
+                        echo "+++++++++Displaying index.html content:+++++++++"
+                        cat index.html
+                    '''
                 }
             }
         }
