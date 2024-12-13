e3dd0f2b5a4a5f0d000ea29f6403ae7cf4f53ee8
Your Name <you@example.com>
2024-12-13 13:30:23 +0530

    change groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,10 @@
 pipeline {
     agent any
+    environment {
+        AWS_REGION = 'us-east-1' // e.g., 'us-east-1'
+        BUCKET_NAME = 'hash2buket'
+        FOLDER_NAME = 'example-folder/' // Include the trailing slash to denote a folder
+    }
 
     stages {
         stage('Check Local File') {
@@ -13,6 +18,19 @@ pipeline {
                         echo "+++++++++Displaying index.html content:+++++++++"
                         cat index.html
                     '''
+                }
+            }
+        }
+    }
+
+    stages {
+        stage('Create Folder in S3') {
+            steps {
+                script {
+                    // Ensure AWS CLI is configured in the Jenkins environment
+                    sh """
+                        aws s3api put-object --bucket $BUCKET_NAME --key $FOLDER_NAME --region $AWS_REGION
+                    """
                 }
             }
         }
